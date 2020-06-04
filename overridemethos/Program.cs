0e0419b35ed0d04f2a72325ac79a4a8d0ebc7a75
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-06-05 00:02:44 +0900

    초기 커밋.

---
+++ overridemethos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overridemethos
+{
+    class Program
+    {
+        
+        int Power(int input)
+        {
+            return input * input;
+        }
+        int Power(int input, int count)
+        {
+            double re= Math.Pow(input, count);
+            return Convert.ToInt32(re);
+        }
+        
+        int SumAll(int end)
+        {
+            int total = 0;
+            for (int i = 0; i <= end; i++)
+            {
+                total += i;
+            }
+            return total;
+        }
+
+        int SumAll(int end, int start)
+        {
+            int total = 0;
+            for (int i = start; i <= end; i++)
+            {
+                total += i;
+            }
+            return total;
+        }
+        
+        static void Main(string[] args)
+        {
+        }
+    }
+}
